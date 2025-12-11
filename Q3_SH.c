92b664c75a2aed136af21e5935daad209c6edffc
SweetKimmi <hevisindatop@gmail.com>
2025-12-11 10:42:05 +0100

    Question3

--- Q3_SH.c
+++ Q3_SH.c
@@ -31,50 +31,7 @@ int main(int argc, char *argv[]) {
     }
 }
 
-
-/* void exeCommand(char *buffer, ssize_t command_size) {
-    // Fortune
-    if (strcmp(buffer, "fortune") == 0){
-        int pid, statuts;
-        pid = fork();
-
-        if (pid!=0){ // Parent process
-            wait(&statuts);
-            }
-        else {
-            execlp("fortune","fortune",(char*)NULL);
-            //const char* fortune_msg= "Today is what happened to yesterday.";
-            //write(STDOUT_FILENO, fortune_msg, strlen(fortune_msg));
-           }
-        }
-
-   // Date
-    else if (strcmp(buffer, "date") == 0){
-        int pid, statuts;
-        pid = fork();
-
-        if (pid!=0){
-            wait(&statuts);
-        }
-        else {
-            execlp("date","date",(char*)NULL);
-        }
-    }
-    
-    // Shell exit
-    else if (strcmp(buffer, "exit") == ) {
-        write(STDOUT_FILENO, "Exit shell...\n", strlen("Exit shell...\n"));
-
-    }
-    
-    // Display
-    else {
-        write(STDOUT_FILENO, buffer, command_size);
-    }
-    return 0;
-}    
-*/
-   
+  
 void exeCommand(char *buffer, ssize_t command_size) {
 
     // Shell exit
