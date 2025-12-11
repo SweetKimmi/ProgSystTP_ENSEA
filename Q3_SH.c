cdf2d8e64ec0f86aa9e63181c99b3d10182b5727
SweetKimmi <hevisindatop@gmail.com>
2025-12-11 10:41:04 +0100

    Question3

---
+++ Q3_SH.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 128
+
+int main(int argc, char *argv[]) {
+    // Welcome message
+    const char* welcome_msg = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.";
+    write(STDOUT_FILENO, welcome_msg, strlen(welcome_msg));
+
+   // Prompt
+    const char* prompt = "\nenseash % ";
+
+    char buffer[BUFFER_SIZE];
+    ssize_t command_size;
+
+    while (1){
+        // Prompt
+        write(STDOUT_FILENO, prompt, strlen(prompt)); 
+
+        // Read
+        command_size = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+        buffer[command_size-1] = '\0'; // replace newline with null terminator
+
+        // Excecution of commands
+        exeCommand(buffer, command_size);
+    }
+}
+
+
+/* void exeCommand(char *buffer, ssize_t command_size) {
+    // Fortune
+    if (strcmp(buffer, "fortune") == 0){
+        int pid, statuts;
+        pid = fork();
+
+        if (pid!=0){ // Parent process
+            wait(&statuts);
+            }
+        else {
+            execlp("fortune","fortune",(char*)NULL);
+            //const char* fortune_msg= "Today is what happened to yesterday.";
+            //write(STDOUT_FILENO, fortune_msg, strlen(fortune_msg));
+           }
+        }
+
+   // Date
+    else if (strcmp(buffer, "date") == 0){
+        int pid, statuts;
+        pid = fork();
+
+        if (pid!=0){
+            wait(&statuts);
+        }
+        else {
+            execlp("date","date",(char*)NULL);
+        }
+    }
+    
+    // Shell exit
+    else if (strcmp(buffer, "exit") == ) {
+        write(STDOUT_FILENO, "Exit shell...\n", strlen("Exit shell...\n"));
+
+    }
+    
+    // Display
+    else {
+        write(STDOUT_FILENO, buffer, command_size);
+    }
+    return 0;
+}    
+*/
+   
+void exeCommand(char *buffer, ssize_t command_size) {
+
+    // Shell exit
+    if (strcmp("exit", buffer) == 0) {
+        write(STDOUT_FILENO, "Exit shell...\n", strlen("Exit shell...\n"));
+        exit(EXIT_FAILURE);
+    }
+    int pid, status;
+    pid = fork();
+    
+    if (pid != 0){ // Parent process
+        wait(&status);
+    }
+    else {
+        execlp(buffer, buffer, (char*)NULL);
+        exit(EXIT_SUCCESS);
+    }
+}    
+
